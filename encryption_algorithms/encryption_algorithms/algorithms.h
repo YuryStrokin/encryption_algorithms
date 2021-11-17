95b7ee218c873088002a0f119a6c19ac136b45dd
Bad_Marat <badamshin1975@yandex.ru>
2021-11-17 17:41:20 +0300

    Создан первичный каркас проекта

---
+++ encryption_algorithms/encryption_algorithms/algorithms.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+namespace algorithms
+{
+	string xorAlgorithms(string&);
+	string rc4Algorithms(string&);
+	string salsa20Algorithms(string&);
+	string rsaAlgorithms(string&);
+}
+#endif // !ALGORITHMS_H
