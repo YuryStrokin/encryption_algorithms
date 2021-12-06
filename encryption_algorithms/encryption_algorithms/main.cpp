61d200180fb7a89aa9253021d11fbbe23e5683a2
Bad_Marat <badamshin1975@yandex.ru>
2021-12-06 18:31:20 +0300

    commit0.3

--- encryption_algorithms/encryption_algorithms/main.cpp
+++ encryption_algorithms/encryption_algorithms/main.cpp
@@ -1,4 +1,5 @@
-#include "algorithms.h"
+#include "BadamshinXOR.h"
+#include "cRC4.h"
 #include <fstream>
 #include <vector>
 
@@ -6,21 +7,28 @@
 
 int main(int argc, char* argv)
 {
-	vector<string>fileNamesInput{"data1.txt", "data2.txt", "data3.txt", "data4.txt"};
-	vector<string>fileNamesOutput{ "result1.txt", "result2.txt", "result3.txt", "result4.txt" };
+	vector<string>fileNamesInput {"data1.txt", "data2.txt", "data3.txt", "data4.txt"};
+	vector<string>fileNamesOutput { "result1.txt", "result2.txt", "result3.txt", "result4.txt" };
+	ifstream fin;
+	ofstream fout;
+	string data;
+	string resultStr;
 	for (size_t i = 0; i < fileNamesInput.size(); i++)
 	{
-		ifstream fin(fileNamesInput[i]);
-		ofstream fout(fileNamesOutput[i]);
+		fin.open(fileNamesInput[i]);
+		fout.open(fileNamesOutput[i]);
 		if (fin.is_open() && fout.is_open())
 		{
 			while (!fin.eof())
 			{
+				getline(fin, data);
 
 			}
 		}
 		else
 			cerr << "ERROR!!! The file named " << fileNamesInput[i] << " or " << fileNamesOutput[i] << " did not open." << endl;
+		fin.close();
+		fout.close();
 	}
 	return 0;
 };
